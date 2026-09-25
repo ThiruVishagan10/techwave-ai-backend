9594e2b60c24f2bc43c4ff9dc1c13959b9a7e974
Thiru Vishagan <thiruvishagan10@gmail.com>
2026-09-26 02:13:19 +0530

    Configure Render deployment and update database SSL handling

--- app/core/config.py
+++ app/core/config.py
@@ -20,6 +20,7 @@ class Settings(BaseSettings):
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "http://localhost:8000",
+        "https://pathbridge-techwaves.vercel.app"
     ]
     
     @field_validator("CORS_ORIGINS", mode="before")
